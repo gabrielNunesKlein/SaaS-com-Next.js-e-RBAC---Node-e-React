068b8d13f4d16f8ea5ff75188f3118d43f6bef36
Gabriel Klein <gabriel.klein@evolutionsoft.com.br>
2025-03-11 23:42:25 -0300

    Chore: fastify/jwt, Feat: autenticated with email & password

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -2,6 +2,7 @@ import { fastify } from 'fastify'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUI from '@fastify/swagger-ui'
 import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
 import {
     jsonSchemaTransform,
     serializerCompiler,
@@ -9,6 +10,7 @@ import {
     ZodTypeProvider
 } from 'fastify-type-provider-zod'
 import { createAccount } from './route/auth/create-account'
+import { autenticationWithPassword } from './route/auth/autenticated-with-password'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -16,24 +18,29 @@ app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifySwagger, {
-    openapi: {
-      info: {
-        title: 'Next.js SaaS',
-        description: 'Full-stack SaaS with multi-tenant & RBAC.',
-        version: '1.0.0',
-      },
-      servers: [],
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'Full-stack SaaS with multi-tenant & RBAC.',
+      version: '1.0.0',
     },
-    transform: jsonSchemaTransform,
-  })
-  
-  app.register(fastifySwaggerUI, {
-    routePrefix: '/docs',
-  })
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
 
 app.register(fastifyCors)
 
 app.register(createAccount)
+app.register(autenticationWithPassword)
 
 app.listen({port: 3333}).then(() => {
     console.log('HTTP server runing!')
