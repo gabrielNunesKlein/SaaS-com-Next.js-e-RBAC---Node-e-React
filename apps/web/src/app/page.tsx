a9a5886cbf387d1005c77fe34051c692cffa11b4
Gabriel Klein <gabriel.klein@evolutionsoft.com.br>
2025-03-19 23:56:34 -0300

    Chore: setup shadcn ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-bold text-5xl">Hello World</div>
+    <Button>
+      Salvar
+    </Button>
   );
 }
